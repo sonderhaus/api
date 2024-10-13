f53cd90fa0965c2812c8954f01b3bd01eba7e5e8
nbr <nikolaus.brunner@protonmail.ch>
2024-10-13 14:06:16 +0200

    feat(deps): add CORS middleware and update dependencies
    - feat(server): implement CORS middleware using `oakCors`
    - chore(deno): add `@tajpouria/cors` dependency to `deno.json` and `deno.lock`

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import { Application } from '@oak';
+import { oakCors } from '@cors';
 import { storiesRouter } from '@/routes/stories.ts';
 import { statusRouter } from '@/routes/status.ts';
 
 const app = new Application();
+
+app.use(oakCors());
 
 app.use(statusRouter.routes());
 app.use(statusRouter.allowedMethods());
